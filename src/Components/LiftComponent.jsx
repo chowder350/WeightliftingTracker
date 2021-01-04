e1c85c52a47fa9f22709286e3aa97884b34b74c3
Carlos Hernandez <chdez350@gmail.com>
2021-01-03 19:51:40 -0600

    finished basic UI for a workout component

--- src/Components/LiftComponent.jsx
+++ src/Components/LiftComponent.jsx
@@ -1,4 +1,7 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class LiftComponent extends Component {
   constructor() {
@@ -10,15 +13,50 @@ class LiftComponent extends Component {
 
 
   render() {
-      console.log(this.props)
+   
     return (
-        <>
-        <h3>{this.props.title}</h3>
-        <label>Sets: </label> <input style={{display:'inline-block'}} value={this.props.sets}/>
-        <label>Reps: </label> <input style={{display:'inline-block'}} value={this.props.reps}/>
-        <label>Weight: </label> <input style={{display:'inline-block'}} value={this.props.weight}/>
-        <label>Notes: </label> <input style={{display:'inline-block'}} type="textbox" value={this.props.notes}/>
-        </>
+        <Form>
+            <h3>{this.props.title}: </h3>
+        <Form.Group as={Row}>
+          <Form.Label column xs="5">
+            Sets:
+          </Form.Label>
+          <Col xs="5">
+            <Form.Control value={this.props.sets}/>
+          </Col>
+        </Form.Group>
+      
+        <Form.Group as={Row}>
+          <Form.Label column xs="5">
+            Reps:
+          </Form.Label>
+          <Col xs="5">
+            <Form.Control value={this.props.reps} />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row}>
+          <Form.Label column xs="5">
+            Weight:
+          </Form.Label>
+          <Col xs="5">
+            <Form.Control value={this.props.weight} />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row}>
+          <Form.Label column xs="5">
+            Notes:
+          </Form.Label>
+          <Col xs="5">
+            <Form.Control type="textarea" value={this.props.notes} />
+          </Col>
+        </Form.Group>
+      </Form>
+
+      
+
+      
     )
   }
 }
--- src/Components/WorkoutComponent.jsx
+++ src/Components/WorkoutComponent.jsx
@@ -19,8 +19,8 @@ class WorkoutComponent extends Component {
     return (
       <>
       <Container>
-        <h2>Push Day</h2>
-        <LiftComponent title={'Bench Press'} sets={3} reps={5} weight={135}/>
+        <h1>Push Day</h1>
+        <LiftComponent title={'Bench Press'} sets={3} reps={5} weight={135} notes={"felt good today"}/>
         </Container>
       </>
     )
