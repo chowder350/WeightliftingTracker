1959307a50ad0b0916e2440497d144b048e5d9b0
Carlos Hernandez <chdez350@gmail.com>
2021-01-07 16:49:54 -0600

    use ssl

--- server/queries.js
+++ server/queries.js
@@ -5,7 +5,8 @@ const pool = new Pool({
   host: process.env.HOST,
   database: process.env.DATABASE,
   password: process.env.PASSWORD,
-  port: process.env.DBPORT
+  port: process.env.DBPORT,
+  ssl: true
 })
 
 console.log(pool);
