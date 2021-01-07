ad48c93c4aa7c327a0be0d6f78de675cf900a3c7
Carlos Hernandez <chdez350@gmail.com>
2021-01-07 16:57:29 -0600

    fix

--- server/queries.js
+++ server/queries.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   database: process.env.DATABASE,
   password: process.env.PASSWORD,
   port: process.env.DBPORT,
-  ssl: true
+  ssl: { rejectUnauthorized: false }
 })
 
 console.log(pool);
